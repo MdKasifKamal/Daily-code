3673380d202d14281b1151771059283a1385e559
MdKasifKamal <mohdkasifkamal680@gmail.com>
2025-10-31 15:47:55 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,13 @@
+alert("kasif")
+console.log("hello world");
+var name = "Kasif";
+let age = 25;
+const country = "india";
+console.log(name , age , country);
+let user = prompt("Enter your name");
+console.log("User name is: " + user);
+let text = "I love html";
+console.log(text.replace("html", "javascript"));
+
+let sentence = "javascript is fun";
+console.log(sentence.includes("fun"));
